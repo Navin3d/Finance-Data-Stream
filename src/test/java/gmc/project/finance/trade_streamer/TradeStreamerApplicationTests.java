bb8e8cd3c3ce42dcc733aa78269eee584a0afe41
navin3d <smnavin65@gmail.com>
2024-09-04 22:39:25 +0530

    Initial Commit

---
+++ src/test/java/gmc/project/finance/trade_streamer/TradeStreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package gmc.project.finance.trade_streamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeStreamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
